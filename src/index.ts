ca9f8a969ffac086db3229288d2a971742b48c09
pure liquid <110220217+pureliquid@users.noreply.github.com>
2022-11-14 19:00:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,4 @@ const fn = async () => {
   const browserService: BrowserService = new BrowserService();
   await browserService.setup();
   const p = await browserService.browser.newPage();
-  /*await browserService.awaitNavigation(p, 'http://192.168.8.1/html/home.html');
-  await p.evaluate(() => {
-    document?.querySelector<HTMLSpanElement>('span#logout_span')?.click();
-    (document?.querySelector<HTMLInputElement>('input#password.input') as HTMLInputElement).value = 'sK24!12$';
-    document.querySelector<HTMLInputElement>('input#pop_login.button_dialog')?.click();
-    return;
-  });
-
-  [...document.querySelectorAll('.user_add_line')[0].querySelectorAll('td')].map(col => col.innerText)
-
-  await p.waitFor(2000);
-  await browserService.awaitNavigation(p, 'http://192.168.8.1/html/virtualserver.html');*/
 };
